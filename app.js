4cd71b310e028d0bdf946eeffd17f91f7e4d5d5b
Adrian <adriansanduta@gmail.com>
2024-11-29 18:09:28 -0600

    1.1

---
+++ app.js
@@ -0,0 +1,9 @@
+// 1. Return the sum of two numbers
+// Create a function that takes two arguments as functions and return their sum
+
+function addition(num1, num2) {
+    return num1 + num2
+}
+console.log(addition(3, 5));
+
+
